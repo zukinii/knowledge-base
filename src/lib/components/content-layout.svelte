b534226a7c94857735cb631c8c6514d2af6b636f
D <17290358+zukinii@users.noreply.github.com>
2023-10-20 02:15:38 +0200

    added some a11y

--- src/lib/components/content-layout.svelte
+++ src/lib/components/content-layout.svelte
@@ -3,8 +3,27 @@
 	import Footer from './footer.svelte'
 </script>
 
+<a href="#main">Skip to main content</a>
 <Nav />
 <main id="main">
 	<slot />
 </main>
 <Footer />
+
+<style>
+	a {
+		position: fixed;
+		top: 0;
+		left: 0;
+		padding: var(--size-3);
+		background-color: var(--bg-1);
+		color: var(--text-1);
+		z-index: var(--layer-skip-link);
+		transform: translateY(-100%);
+		transition: transform var(--transition);
+
+		&:focus-visible {
+			transform: translateY(0);
+		}
+	}
+</style>
