f91634840a11c93dc3503fd194a5882168bf05a7
D <17290358+zukinii@users.noreply.github.com>
2023-11-13 15:02:55 +0100

    notes on a thank-you page
    subject to change

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Thank you!</h1>
+
+<p>This page will feature links to</p>
+
+<ul>
+	<li>everyone who supports the project</li>
+	<li>the art, music and marketing people who made it possible</li>
+	<li>some placeholder page to myself in case I make other projects in the future</li>
+</ul>
