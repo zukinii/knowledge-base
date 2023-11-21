e5478cc5f334bb0e3c50a33965f6c47261697fa7
D <17290358+zukinii@users.noreply.github.com>
2023-11-21 16:31:12 +0100

    basic feature detection page

---
+++ src/routes/features/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from 'svelte'
+
+	let features = []
+
+	onMount(async () => {
+		// Check for IndexedDB support
+		const indexedDBSupported = 'indexedDB' in window
+
+		// Check for Storage Estimation support
+		const storageEstimationSupported = 'storage' in navigator && 'estimate' in navigator.storage
+
+		features = [
+			{ name: 'IndexedDB', supported: indexedDBSupported },
+			{ name: 'Storage Estimation', supported: storageEstimationSupported }
+		]
+	})
+</script>
+
+<h1>Available Features</h1>
+
+<ul>
+	{#each features as feature}
+		<li>
+			{#if feature.supported}
+				✅ {feature.name} is supported
+			{:else}
+				❌ {feature.name} is not supported
+			{/if}
+		</li>
+	{/each}
+</ul>
